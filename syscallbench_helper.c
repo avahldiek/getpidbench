350ccc24ace12518b60dc06c688f1a0692695846
Anjo Vahldiek-Oberwagner <vahldiek@mpi-sws.org>
2016-07-27 14:18:47 +0200

    check for errors

--- syscallbench_helper.c
+++ syscallbench_helper.c
@@ -5,19 +5,26 @@
  *      Author: vahldiek
  */
 
+#include <stdio.h>
 #include <unistd.h>
 #include <sys/syscall.h>
 #include <cputimer.h>
 
 unsigned long long run_syscall_bench(unsigned int sc, unsigned int count) {
 	unsigned int i = 0;
+	int ret = 0;
 	unsigned long long start = rdtsc();
 
 	for (i = 0; i < count; i++) {
-		syscall(sc);
+		ret |= syscall(sc);
 	}
 
 	unsigned long long end = rdtsc();
+
+	if(ret != 0) {
+		printf("Syscall failed ret=%d\n", ret);
+		perror("Syscall failed:");
+	}
 
 	return end - start;
 }
